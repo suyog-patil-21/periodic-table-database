b80bc569b2912a8c1c2cc990d05a97f33398247d
Suyog Patil <suyog0057@gmail.com>
2024-06-21 15:59:33 +0000

    chore: added todo comments

--- element.sh
+++ element.sh
@@ -3,6 +3,18 @@
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-else 
-  echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+else
+  LENGTH_OF_INPUT=$(echo "$1" | wc -m)
+  LENGTH_OF_INPUT=$(( LENGTH_OF_INPUT - 1))
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+  # For Input=Atomic number
+  else if [[ $LENGTH_OF_INPUT -le 2 ]]
+  # For Input=Element Symbol
+  else
+  # For Input=Element Name
+  fi
+  # Setting Variables
+  
+  echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ELEMENT_MASS amu. $ELEMENT_NAME has a melting point of $ELEMENT_MELTING_POINT celsius and a boiling point of $ELEMENT_BOILING_POINT celsius."
 fi
